7a7a7b89fb19a9eb4b56fef5fd0fe2dd02cf641a
Tim Otten <totten@civicrm.org>
2020-09-08 03:29:21 -0700

    Move TaskList::pickWeights() to PackageSorter::sortPackages()

---
+++ src/PackageSorter.php
@@ -0,0 +1,108 @@
+<?php
+namespace Civi\CompilePlugin;
+
+use Composer\Package\PackageInterface;
+
+class PackageSorter {
+
+    /**
+     * Get the list of installed packages (sorted topologically).
+     *
+     * @param PackageInterface[] $installedPackages
+     *   All installed packages, including the root.
+     * @return array
+     *   List of installed packages (sorted topologically).
+     *
+     *   Upstream packages with no dependencies come earlier than downstream packages that require them.
+     *
+     *   Ex: [0 => 'very-much/upstream', 1 => 'some-what/midstream', 2 => 'hear-now/downstream']
+     */
+    public static function sortPackages($installedPackages) {
+        // We do our own topological sort. It doesn't seem particularly easy
+        // to ask composer for this list -- the code-paths for 'composer require'
+        // and 'composer update' have to address a lot of issues (like version-selection)
+        // that don't matter. We're using whatever packages are already here.
+
+        // Given that $allPackages starts in some known order, the remaining steps
+        // should be deterministic/stable over multiple iterations.
+        usort($installedPackages, function($a, $b) {
+            return strnatcmp($a->getName(), $b->getName());
+        });
+
+        // Valid names for the installed packages, inclusive of aliases.
+        // Array(string $packageName => bool)
+        $validNames = [];
+        foreach ($installedPackages as $package) {
+            /** @var PackageInterface $package */
+            $validNames[$package->getName()] = TRUE;
+            foreach ($package->getProvides() as $link) {
+                $validNames[$link->getTarget()] = TRUE;
+            }
+            foreach ($package->getReplaces() as $link) {
+                $validNames[$link->getTarget()] = TRUE;
+            }
+        }
+
+        // Any unrecognized/virtualized packages (e.g. PECL) that we should ignore.
+        // Array(string $package => bool)
+        $ignoredNames = [];
+        foreach ($installedPackages as $package) {
+            /** @var PackageInterface $package */
+            foreach ($package->getRequires() as $link) {
+                if (!isset($validNames[$link->getTarget()])) {
+                    $ignoredNames[$link->getTarget()] = TRUE;
+                }
+            }
+        }
+
+        // Unsorted list of packages that need to be visited.
+        // Array(string $packageName => PackageInterface $package).
+        $todoPackages = [];
+        foreach ($installedPackages as $package) {
+            $todoPackages[$package->getName()] = $package;
+        }
+
+        // The topologically sorted packages, from least-dependent to most-dependent.
+        // Array(string $packageName => PackageInterface $package)
+        $sortedPackages = [];
+
+        // A package is "ripe" when all its requirements are met.
+        $isRipe = function(PackageInterface $pkg) use (&$sortedPackages, &$ignoredNames) {
+            foreach ($pkg->getRequires() as $link) {
+                if (!isset($sortedPackages[$link->getTarget()]) && !isset($ignoredNames[$link->getTarget()])) {
+                    // printf("[%s] is not ripe due to [%s]\n", $pkg->getName(), $link->getTarget());
+                    return FALSE;
+                }
+            }
+            // printf("[%s] is ripe\n", $pkg->getName());
+            return TRUE;
+        };
+
+        // A package is "consumed" when we move it from $todoPackages to $sortedPackages.
+        $consumePackage = function(PackageInterface $pkg) use (&$sortedPackages, &$todoPackages, &$ignoredNames) {
+            $sortedPackages[$pkg->getName()] = $pkg;
+            unset($todoPackages[$pkg->getName()]);
+
+            foreach ($pkg->getProvides() as $link) {
+                $ignoredNames[$link->getTarget()] = TRUE;
+            }
+            foreach ($pkg->getReplaces() as $link) {
+                $ignoredNames[$link->getTarget()] = TRUE;
+            }
+        };
+
+        // Main loop: Progressively move ripe packages from $todoPackages to $sortedPackages.
+        while (!empty($todoPackages)) {
+            $ripePackages = array_filter($todoPackages, $isRipe);
+            if (empty($ripePackages)) {
+                throw new \RuntimeException("Error: Failed to find next installable package.");
+            }
+            foreach ($ripePackages as $package) {
+                $consumePackage($package);
+            }
+        }
+
+        return array_keys($sortedPackages);
+    }
+
+}
--- src/TaskList.php
+++ src/TaskList.php
@@ -3,11 +3,7 @@ namespace Civi\CompilePlugin;
 
 
 use Composer\Composer;
-use Composer\DependencyResolver\DefaultPolicy;
-use Composer\DependencyResolver\Pool;
 use Composer\Package\PackageInterface;
-use Composer\Repository\PlatformRepository;
-use Composer\Semver\Constraint\Constraint;
 
 class TaskList
 {
@@ -24,10 +20,10 @@ class TaskList
 
     public function load(Composer $composer) {
         $this->tasks = [];
-        $this->packageWeights = $this->pickWeights(array_merge(
+        $this->packageWeights = array_flip(PackageSorter::sortPackages(array_merge(
           $composer->getRepositoryManager()->getLocalRepository()->getCanonicalPackages(),
           [$composer->getPackage()]
-        ));
+        )));
 
         $this->loadPackage($composer, $composer->getPackage(), realpath('.'));
         // I'm not a huge fan of using 'realpath()' here, but other tasks (using `getInstallPath()`)
@@ -37,103 +33,6 @@ class TaskList
         foreach ($localRepo->getCanonicalPackages() as $package) {
             $this->loadPackage($composer, $package, $composer->getInstallationManager()->getInstallPath($package));
         }
-    }
-
-    /**
-     * Get the list of installed packages (sorted topologically).
-     *
-     * @param PackageInterface[] $allPackages
-     *   All installed packages, including the root.
-     * @return array
-     *   List of installed packages (sorted topologically).
-     *
-     *   Upstream packages with no dependencies come earlier than downstream packages that require them.
-     *
-     *   Ex: ['very-much/upstream' => 0, 'some-what/midstream' => 1, 'hear-now/downstream' => 2]
-     */
-    protected function pickWeights($allPackages) {
-        // Given that $allPackages starts in some known order, the remaining steps
-        // should be deterministic/stable over multiple iterations.
-        usort($allPackages, function($a, $b) {
-            return strnatcmp($a->getName(), $b->getName());
-        });
-
-        // Valid names for the installed packages, inclusive of aliases.
-        // Array(string $packageName => string $samePackageName)
-        $validPackages = [];
-        foreach ($allPackages as $package) {
-            /** @var PackageInterface $package */
-            $validPackages[$package->getName()] = $package->getName();
-            foreach ($package->getProvides() as $link) {
-                $validPackages[$link->getTarget()] = $link->getTarget();
-            }
-            foreach ($package->getReplaces() as $link) {
-                $validPackages[$link->getTarget()] = $link->getTarget();
-            }
-        }
-
-        // Any unrecognized/virtualized packages (e.g. PECL) that we should ignore.
-        // Array(string $package => string $samePackageName)
-        $ignoredPackages = [];
-        foreach ($allPackages as $package) {
-            /** @var PackageInterface $package */
-            foreach ($package->getRequires() as $link) {
-                if (!isset($validPackages[$link->getTarget()])) {
-                    $ignoredPackages[$link->getTarget()] = $link->getTarget();
-                }
-            }
-        }
-
-        // Unsorted list of packages that need to be visited.
-        // Array(string $packageName => PackageInterface $package).
-        $todoPackages = [];
-        foreach ($allPackages as $package) {
-            $todoPackages[$package->getName()] = $package;
-        }
-
-        // The topologically sorted packages, from least-dependent to most-dependent.
-        // Array(string $packageName => string $samePackageName)
-        $sortedPackages = [];
-
-        // A package is "ripe" when all its requirements are met.
-        $isRipe = function(PackageInterface $pkg) use (&$sortedPackages, &$ignoredPackages) {
-            foreach ($pkg->getRequires() as $link) {
-                if (!isset($sortedPackages[$link->getTarget()]) && !isset($ignoredPackages[$link->getTarget()])) {
-                    // printf("[%s] is not ripe due to [%s]\n", $pkg->getName(), $link->getTarget());
-                    return FALSE;
-                }
-            }
-            // printf("[%s] is ripe\n", $pkg->getName());
-            return TRUE;
-        };
-
-        // A package is "consumed" when we move it from $todoPackages to $sortedPackages.
-        $consumePackage = function(PackageInterface $pkg) use (&$sortedPackages, &$todoPackages, &$ignoredPackages) {
-            $sortedPackages[$pkg->getName()] = $pkg->getName();
-            unset($todoPackages[$pkg->getName()]);
-
-            foreach ($pkg->getProvides() as $link) {
-                $ignoredPackages[$link->getTarget()] = $link->getTarget();
-            }
-            foreach ($pkg->getReplaces() as $link) {
-                $ignoredPackages[$link->getTarget()] = $link->getTarget();
-            }
-        };
-
-        // Main loop
-        while (!empty($todoPackages)) {
-            $ripePackages = array_filter($todoPackages, $isRipe);
-            if (empty($ripePackages)) {
-                throw new \RuntimeException("Error: Failed to find next installable package.");
-            }
-            foreach ($ripePackages as $package) {
-                $consumePackage($package);
-            }
-        }
-
-        $result = array_flip(array_values($sortedPackages));
-        // printf("sorted: %s\n", implode(' ', $sortedPackages));
-        return $result;
     }
 
     /**
