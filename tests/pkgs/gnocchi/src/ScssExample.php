a3baae5a1a164f242821434eb43217fb52b7e67d
Tim Otten <totten@civicrm.org>
2020-09-12 04:22:54 -0700

    ScssExample - Simplify. Rely on PWD.

--- tests/pkgs/gnocchi/src/ScssExample.php
+++ tests/pkgs/gnocchi/src/ScssExample.php
@@ -5,11 +5,10 @@ class ScssExample
 {
     public static function make(array $task)
     {
-        $pkgDir = dirname(__DIR__);
         $scssCompiler = new \ScssPhp\ScssPhp\Compiler();
         $scss = 'div { .foo { hyphens: auto; } }';
         $css = $scssCompiler->compile($scss);
         $autoprefixer = new \Padaliyajay\PHPAutoprefixer\Autoprefixer($css);
-        file_put_contents("$pkgDir/build.css", $autoprefixer->compile());
+        file_put_contents("build.css", $autoprefixer->compile());
     }
 }
