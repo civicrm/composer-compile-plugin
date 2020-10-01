1f9aaa429b7d2fdc3b7b86513aa56f8f5032c06e
Tim Otten <totten@civicrm.org>
2020-09-30 23:13:32 -0700

    CompilePlugin - The TaskList+TaskRunner should launch in subprocesses (#7)

--- src/Command/CompileCommand.php
+++ src/Command/CompileCommand.php
@@ -4,6 +4,7 @@ namespace Civi\CompilePlugin\Command;
 
 use Civi\CompilePlugin\TaskList;
 use Civi\CompilePlugin\TaskRunner;
+use Composer\Script\ScriptEvents;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -21,6 +22,7 @@ class CompileCommand extends \Composer\Command\BaseCommand
           ->setDescription('Run compilation tasks')
           ->addOption('all', null, InputOption::VALUE_NONE, 'Run all tasks, regardless of configuration')
           ->addOption('dry-run', 'N', InputOption::VALUE_NONE, 'Dry-run: Print a list of steps to be run')
+          ->addOption('soft-options', null, InputOption::VALUE_OPTIONAL, '(Internal)')
           ->addArgument('filterExpr', InputArgument::IS_ARRAY, 'Optional filter to match. Ex: \'vendor/package\' or \'vendor/package:id\'')
           ->setHelp(
               "Run compilation steps in all packages\n" .
@@ -29,6 +31,23 @@ class CompileCommand extends \Composer\Command\BaseCommand
               "configuration (per composer.json and environment-variables)."
           )
         ;
+    }
+
+    protected function initialize(
+        InputInterface $input,
+        OutputInterface $output
+    ) {
+        $so = $input->getOption('soft-options');
+        if ($so) {
+            $json = json_decode(base64_decode($so), 1);
+            foreach ($json['o'] ?? [] as $key => $value) {
+                if ($input->hasOption($key)) {
+                    $input->setOption($key, $value);
+                }
+            }
+        }
+
+        parent::initialize($input, $output);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
